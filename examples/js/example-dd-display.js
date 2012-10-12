ea8b60d8d35263f029318f9da814849abf7fb95e
Jon Arne Sæterås <seteras@gmail.com>
2012-10-12 11:43:43 +0200

    moved examples to examples

---
+++ examples/js/example-dd-display.js
@@ -0,0 +1,39 @@
+var MTDD = new MTDD('finn');
+
+function appendOption(text)
+{
+
+  var elOptNew = document.createElement('option');
+  elOptNew.text = text;
+  elOptNew.value = text;
+
+  var elSel = document.getElementById('capas');
+
+  try {
+    elSel.add(elOptNew, null); // standards compliant; doesn't work in IE
+  }
+  catch(ex) {
+    elSel.add(elOptNew); // IE only
+  }
+}
+
+function capaselect() {
+	var elSel = document.getElementById('capas');
+
+	var para = document.getElementById('capaValue');
+
+	
+	var capaValue = MTDD.getCapability(elSel.value);
+
+	if(capaValue) {
+		para.innerHTML = capaValue;
+	}
+}
+
+var capas = MTDD.getCapabilities();
+
+if(capas) {
+	for (var key in capas) {
+		appendOption(key);
+	}
+}
